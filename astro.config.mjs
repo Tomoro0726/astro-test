04fba7662f7f4eb064e5edbb8a29b7370ceccd4e
Tomoro0726 <tomoro0726@gmail.com>
2024-10-18 09:41:00 +0900

    go

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,4 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({
-  build: {
-    format: "file",
-    assets: "_custom",
-  },
-  base: "/astro-test",
-  site: "https://tomoro0726.github.io",
-});
+export default defineConfig({});
